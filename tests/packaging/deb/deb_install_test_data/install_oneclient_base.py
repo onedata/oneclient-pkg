efb97ba1c7f8a87afc5389e8309363116997a33f
Bartek Kryza <bkryza@gmail.com>
2022-01-27 20:37:03 +0100

    VFS-8872 Dropped py2 package tests

--- tests/packaging/deb/deb_install_test_data/install_oneclient_base.py
+++ tests/packaging/deb/deb_install_test_data/install_oneclient_base.py
@@ -11,10 +11,6 @@ oneclient_package = [path for path in packages
                      and (dist in path)
                      and not path.startswith('oneclient-base-debuginfo')][0]
 
-onedatafs_py2_package = [path for path in packages
-                         if path.startswith('python-onedatafs')
-                         and (dist in path)][0]
-
 onedatafs_py3_package = [path for path in packages
                          if path.startswith('python3-onedatafs')
                          and (dist in path)][0]
@@ -24,19 +20,12 @@ onedatafs_py3_package = [path for path in packages
 check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=oneclient_package)
             ], stderr=STDOUT)
 
-# install onedatafs Python2 package
-check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=onedatafs_py2_package)
-            ], stderr=STDOUT)
-
 # install onedatafs Python3 package
 check_call(['sh', '-c', 'apt -y install /root/pkg/{package}'.format(package=onedatafs_py3_package)
             ], stderr=STDOUT)
 
 # validate oneclient package installation
 check_call(['/usr/bin/oneclient', '--help'])
-
-# validate onedatafs Python2 package installation
-check_call(['python2', '-c', 'from onedatafs import OnedataFS'])
 
 # validate onedatafs Python3 package installation
 check_call(['python3', '-c', 'from onedatafs import OnedataFS'])
