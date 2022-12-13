1581709dfa85e0817cd81070cbf1a5e5423c32ee
Darin Nikolow <darnik22@gmail.com>
2022-12-13 13:15:57 +0000

    VFS-9622 Removing old glusterfs rpms before testing

--- tests/packaging/rpm/rpm_install_test_data/install_fsonedatafs.py
+++ tests/packaging/rpm/rpm_install_test_data/install_fsonedatafs.py
@@ -14,6 +14,8 @@ fsonedatafs_py3_package = [path for path in packages
 check_call(['yum', '-y', 'install', 'python36-pip'])
 check_call(['pip3', 'install', 'setuptools', 'six', 'fs'])
 
+#check_call(['sleep', '360000']) # FIXME: Remove after testing
+
 # install onedatafs packages for Python3
 check_call(['yum', '-y', '--enablerepo=onedata', 'install',
             '/root/pkg/' + onedatafs_py3_package], stderr=STDOUT)
@@ -21,6 +23,12 @@ check_call(['yum', '-y', '--enablerepo=onedata', 'install',
 # install fsonedatafs packages for Python3
 check_call(['yum', '-y', '--enablerepo=onedata', 'install',
             '/root/pkg/' + fsonedatafs_py3_package], stderr=STDOUT)
+
+# Remove glusterfs libs comming from onedata2102 repo
+# After publishing stable 21.02 the libs should be removed from repo 
+# as well as the erasing check_call here
+# FIXME: Remove after publishing stable relesae 21.02
+check_call(['yum', '-y', 'erase', 'onedata2102-glusterfs-libs', 'onedata2102-glusterfs', 'onedata2102-glusterfs-client-xlators', 'onedata2102-glusterfs-api'])
 
 # validate onedatafs Python3 package installation
 check_call(['scl', 'enable', 'onedata{release}'.format(release='2102'), 'PYTHONPATH="${ONEDATA_PYTHON3_PATH}" python3 -c "from fs.onedatafs import OnedataFS"'])
