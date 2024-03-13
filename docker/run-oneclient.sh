f3b9ac25425d238c457cd39dbc4bbc136751a684
Łukasz Opioła <ymopiola@cyfronet.pl>
2024-03-13 20:57:34 +0100

    VFS-11629 Use the --force-proxy-io flag in demo mode

--- docker/run-oneclient.sh
+++ docker/run-oneclient.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-if [ $1"x" == "demox" ]; then
+if [ "$1x" == "demox" ]; then
     source /root/demo-mode/setup-demo.sh "${2}" "${3}"
-    /opt/oneclient/bin/oneclient -f /mnt/oneclient
+    /opt/oneclient/bin/oneclient -f /mnt/oneclient --force-proxy-io
 else
     /opt/oneclient/bin/oneclient -f "$@"
 fi
-
-
